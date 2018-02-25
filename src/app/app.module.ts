6f05de0317262c1835e2cdec3819834c0c3d901f
Jennifer GULLUNG <jenny@gullung.com>
2018-02-25 21:46:25 +0100

    correction bug camera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { IonicStorageModule } from '@ionic/storage';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 import { MyApp } from './app.component';
 import { ListPage } from '../pages/list/list';
@@ -44,7 +45,8 @@ import { PizzaAdminPage } from "../pages/pizzaAdmin/pizzaAdmin";
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Camera
   ]
 })
 
